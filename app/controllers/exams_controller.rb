5b7702862655f1af3ad1be569cbf15f29b4d918f
tamilcrea@gmail.com <tamilcrea@gmail.com>
2019-04-24 12:09:57 +0530

    added controller logic

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,2 +1,14 @@
 class ExamsController < ApplicationController
-end
+
+	def index
+		@exams = @Exams.all
+		render json @exam
+    end
+
+    def create
+    	@exams = Exams.new(exam_params)
+    end
+
+  private exam_params
+  	params.require(:exams).permit(:exam_id)
+  end	
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,2 +1,15 @@
 class TopicsController < ApplicationController
-end
+	
+	def index
+	 @topic = Topics.all
+    end
+
+    def create
+     @topic = Topics.new(topic_params)
+    end
+
+
+private topic_params
+   def topic_params
+   	params.require(:topic).permit(:topic_name)
+   end
