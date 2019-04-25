9065e4fd7dbd297301836f724163c3b1bd3f6913
tamilcrea@gmail.com <tamilcrea@gmail.com>
2019-04-25 22:30:33 +0530

    seeding the database

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_25_093233) do
+ActiveRecord::Schema.define(version: 2019_04_25_094726) do
 
   create_table "chapters", force: :cascade do |t|
     t.integer "chapter_id"
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+50 times.do
+User.create(
+	username: Faker::Name.name_with_middle,
+	email: Faker::Internet.free_email
+)
+end
